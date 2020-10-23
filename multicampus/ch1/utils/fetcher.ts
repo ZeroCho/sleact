f4f30065096fda670ac73b96528dee7f95914fbe
ZeroCho <zerohch0@gmail.com>
2020-10-23 16:02:40 +0900

    Add: favicon

--- multicampus/ch1/utils/fetcher.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const fetcher = (url: string) => axios.get(url, { withCredentials: true }).then((response) => response.data);
-
-export default fetcher;
--- multicampus/setting/webpack.config.ts
+++ multicampus/setting/webpack.config.ts
@@ -87,7 +87,7 @@ const config: webpack.Configuration = {
 if (isDevelopment && config.plugins) {
   config.plugins.push(new webpack.HotModuleReplacementPlugin());
   config.plugins.push(new ReactRefreshWebpackPlugin());
-  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: false }));
 }
 if (!isDevelopment && config.plugins) {
   config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
