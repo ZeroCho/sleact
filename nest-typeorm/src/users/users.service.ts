aec43c02a61f206d26eee33c905e649ce297a98d
ZeroCho <zerohch0@gmail.com>
2021-09-12 14:47:02 +0900

    Update: transaction code

--- nest-typeorm/src/users/users.service.ts
+++ nest-typeorm/src/users/users.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import bcrypt from 'bcrypt';
 import { ChannelMembers } from '../entities/ChannelMembers';
 
@@ -15,6 +15,7 @@ export class UsersService {
     private workspaceMembersRepository: Repository<WorkspaceMembers>,
     @InjectRepository(ChannelMembers)
     private channelMembersRepository: Repository<ChannelMembers>,
+    private connection: Connection,
   ) {}
 
   async findByEmail(email: string) {
@@ -25,24 +26,42 @@ export class UsersService {
   }
 
   async join(email: string, nickname: string, password: string) {
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = await this.usersRepository.findOne({ where: { email } });
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    const user = await queryRunner.manager
+      .getRepository(Users)
+      .findOne({ where: { email } });
     if (user) {
-      return false;
+      throw new UnauthorizedException('이미 존재하는 사용자입니다');
     }
-    const returned = await this.usersRepository.save({
-      email,
-      nickname,
-      password: hashedPassword,
-    });
-    const workspaceMember = new WorkspaceMembers();
-    workspaceMember.UserId = returned.id;
-    workspaceMember.WorkspaceId = 1;
-    await this.workspaceMembersRepository.save(workspaceMember);
-    const channelMember = new ChannelMembers();
-    channelMember.UserId = returned.id;
-    channelMember.ChannelId = 1;
-    await this.channelMembersRepository.save(channelMember);
-    return true;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    try {
+      const returned = await queryRunner.manager.getRepository(Users).save({
+        email,
+        nickname,
+        password: hashedPassword,
+      });
+      const workspaceMember = queryRunner.manager
+        .getRepository(WorkspaceMembers)
+        .create();
+      workspaceMember.UserId = returned.id;
+      workspaceMember.WorkspaceId = 1;
+      await queryRunner.manager
+        .getRepository(WorkspaceMembers)
+        .save(workspaceMember);
+      await queryRunner.manager.getRepository(ChannelMembers).save({
+        UserId: returned.id,
+        ChannelId: 1,
+      });
+      await queryRunner.commitTransaction();
+      return true;
+    } catch (error) {
+      console.error(error);
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
