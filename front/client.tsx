559b7757fbae959b7995a1c92bb78ab9dfb16cdd
ZeroCho <zerohch0@gmail.com>
2022-01-05 12:56:42 +0900

    Update: swr-detvools

--- front/client.tsx
+++ front/client.tsx
@@ -4,8 +4,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
-import SWRDevtools, { Cache } from '@jjordy/swr-devtools';
-import { SWRConfig } from 'swr';
+import SWRDevtools from '@jjordy/swr-devtools';
 
 import App from './layouts/App';
 
@@ -15,10 +14,13 @@ axios.defaults.baseURL =
 console.log('env', process.env.NODE_ENV === 'production');
 render(
   <BrowserRouter>
-    <SWRConfig value={{ provider: () => new Cache() }}>
-      {process.env.NODE_ENV === 'production' ? null : <SWRDevtools />}
+    {process.env.NODE_ENV === 'production' ? (
       <App />
-    </SWRConfig>
+    ) : (
+      <SWRDevtools>
+        <App />
+      </SWRDevtools>
+    )}
   </BrowserRouter>,
   document.querySelector('#app'),
 );
